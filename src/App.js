bbf8c2fc52ef5d1362122477ed1f7f2daaca74bd
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2022-02-01 02:21:00 +0530

    NewsApp

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  const pageSize = 15;
+  const apiKey = process.env.REACT_APP_NEWS;
+
+  const [progress, setProgress] = useState(0);
+
+
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+        <Routes>
+          <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path='/general' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="" pageSize={pageSize} country="in" category="science" />} />
+          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="sports" />} />
+          <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
